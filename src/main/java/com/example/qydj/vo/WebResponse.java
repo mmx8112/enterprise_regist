d4dcdf1b2e3d6384518b4425c6e5dbeb5640d54b
mamx <liushuai@gaosiedu.com>
2022-04-07 17:15:33 +0800

    企业登记修改出参

--- src/main/java/com/example/qydj/vo/WebResponse.java
+++ src/main/java/com/example/qydj/vo/WebResponse.java
@@ -1,6 +1,7 @@
 package com.example.qydj.vo;
 
 import com.example.qydj.dto.CResponseBody;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -10,9 +11,17 @@ import lombok.Data;
  */
 @Data
 public class WebResponse {
+
+    @JsonProperty("C-Response-Desc")
     private String responseDesc = "success";
+
+    @JsonProperty("C-API-Status")
     private String apiStatus = "00";
+
+    @JsonProperty("C-Response-Body")
     private CResponseBody responseBody;
+
+    @JsonProperty("C-Response-Code")
     private String responseCode = "000000000000";
 
 
